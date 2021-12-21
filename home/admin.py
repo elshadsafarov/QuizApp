92b832ebaa2f8e4f30d500f82f8318ac9bca4a50
LeilaHuseinova9 <leilahuseinova9@gmail.com>
2021-12-21 21:59:44 +0400

    Leila's commit

---
+++ home/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import Quiz, Question, Answer
+
+admin.site.register(Quiz)
+
+
+class AnswerInLine(admin.TabularInline):
+    model = Answer
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
+    
